a8e925f5474ba13f43a9b156c1a8559a399b0687
ischeglov <Ivan_mix@mail.ru>
2023-08-30 11:04:26 +0300

    comments fix

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -7,12 +7,7 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> {
-        if (y != 0) return x / y;
-        else {
-            throw new ArithmeticException("Ошибка! Делить на 0 нельзя!");
-        }
-    };
+    BinaryOperator<Integer> devide = (x, y) -> y != 0 ? x / y : 0;
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
